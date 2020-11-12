d2cb9d4e13a6cb988a458303e84fad6993cdcab5
vbaobao <vbao.main@gmail.com>
2020-11-11 19:23:26 -0800

    Complete findNRooksSolution

--- src/solvers.js
+++ src/solvers.js
@@ -16,15 +16,38 @@
 
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  var numRows = n;
+  var solution = new Board({n: numRows});
+  // n x n we assume we have array matrix
+  // assume n : rows, n : cols
 
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  for ( var x = 0; x < n; x++) { // Traverse to x axis
+    for (var y = 0; y < n; y++) { // Traverse y axis
+      if (solution.rows()[x][y] === 1) {
+        break;
+      }
+      solution.rows()[x][y] = 1;
+      if (solution.hasAnyRowConflicts()) {
+        // If there is row conflict
+        console.log('There is a row conflict');
+        solution.rows()[x][y] = 0;
+        break;
+      }
+      if (solution.hasAnyColConflicts()) {
+        // If there is col conflict
+        console.log('There is a col conflict');
+        solution.rows()[x][y] = 0;
+        continue;
+      }
+    }
+  }
+  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution.rows()));
+  return solution.rows();
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 0;//undefined; //fixme
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
